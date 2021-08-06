049f6bddbf2f6d8ced09d080e08218bd82c4cf37
GinBe <asdfwxwx@gmail.com>
2021-08-06 21:42:42 +0800

    ColumnList

--- zheye/src/App.vue
+++ zheye/src/App.vue
@@ -1,10 +1,16 @@
 <template>
-  <div id="nav">
+  <!-- <div class="container">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  </div> -->
+  <div class="container"></div>
+  <router-view />
 </template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.min.css";
+export default {};
+</script>
 
 <style lang="scss">
 #app {
---
+++ zheye/src/components/ColumnList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+      <ul>
+          <li v-for="column in columns" :key="column.id">
+              <img :src="column.avatar" :alt="column.title">
+              <h5>{{column.title}}</h5>
+              <p>{{column.description}}</p>
+              <a href="#">进入专栏……</a>
+          </li>
+      </ul>
+  </div>
+</template>
+<script>
+import {DefineComponent,PropType} from 'vue',
+export interface ColumnProps{
+    id: number,
+    title: string,
+    avatar: string,
+    description: string,
+}
+export default {
+    name: 'ColumnProps',
+    props: {
+        list:{
+            type:Array as PropType<ColumnProps[]>
+            required:true
+        }
+    },
+    // setup(props){
+    //     props.list[0].avatar
+    // }
+}
+</script>
