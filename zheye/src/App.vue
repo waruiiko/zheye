e838636192b1658adc5723e2a9dd8a39cc82e2a1
GinBe <asdfwxwx@gmail.com>
2021-08-06 22:41:53 +0800

    columnlist

--- zheye/src/App.vue
+++ zheye/src/App.vue
@@ -3,34 +3,39 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div> -->
-  <div class="container"></div>
-  <router-view />
+  <div class="container">
+    <column-list :list="list"></column-list>
+  </div>
+  <!-- <router-view /> -->
 </template>
 
-<script>
+<script lang="ts">
 import "bootstrap/dist/css/bootstrap.min.css";
-export default {};
-</script>
+import ColumnList, { ColumnProps } from "./components/ColumnList.vue";
+import { defineComponent } from "vue";
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'test1的专栏',
+    description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  },
+  {
+    id: 2,
+    title: 'test2的专栏',
+    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
   }
-}
-</style>
+]
+
+export default defineComponent({
+  name: "App",
+  components: { ColumnList },
+  setup() {
+    return {
+      list: testData,
+    };
+  },
+})
+</script>
