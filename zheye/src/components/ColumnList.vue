90d751e89df0d099272d9ea29173920735c30d60
GinBe <asdfwxwx@gmail.com>
2021-08-07 22:34:55 +0800

    require

--- zheye/src/components/ColumnList.vue
+++ zheye/src/components/ColumnList.vue
@@ -1,33 +1,57 @@
 <template>
   <!-- <div> -->
-    <ul>
-      <li v-for="column in list" :key="column.id">
-        <img :src="column.avatar" :alt="column.title" />
-        <h5>{{ column.title }}</h5>
-        <p>{{ column.description }}</p>
-        <a href="#">进入专栏……</a>
-      </li>
-    </ul>
+  <div class="row">
+    <div v-for="column in list" :key="column.id" class="col-4 mb-4">
+      <div class="card h-100 shadow-sm">
+        <div class="card-body text-center">
+          <img
+            :src="column.avatar"
+            class="rounded-circle border border-light w-25 my-3"
+            :alt="column.title"
+          />
+          <h5 class="card-title">{{ column.title }}</h5>
+          <p class="card-text text-left">{{ column.description }}</p>
+          <a href="#" class="btn btn-outline-primary">进入专栏</a>
+        </div>
+      </div>
+    </div>
+  </div>
   <!-- </div> -->
 </template>
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
+import { computed, defineComponent, PropType } from "vue";
 export interface ColumnProps {
-    id: number,
-    title: string,
-    avatar: string,
-    description: string,
+  id: number;
+  title: string;
+  avatar?: string;
+  description: string;
 }
 export default defineComponent({
-    name: 'ColumnList',
-    props: {
-            list: {
+  name: "ColumnList",
+  props: {
+    list: {
       //这里特别有一点，我们现在的 Array 是没有类型的，只是一个数组，我们希望它是一个 ColomnProps 的数组，那么我们是否可以使用了类型断言直接写成 ColomnProps[]，显然是不行的 ，因为 Array 是一个数组的构造函数不是类型，我们可以使用 PropType 这个方法，它接受一个泛型，讲 Array 构造函数返回传入的泛型类型。
       type: Array as PropType<ColumnProps[]>,
-      required: true
-    }
+      required: true,
+    },
     // setup(props){
     //     props.list[0].avatar
     // }
-}})
+  },
+  setup(props) {
+    const columnList = computed(() => {
+      return props.list.map(column => {
+        if (!column.avatar) {
+          column.avatar = require('@/assets/logo.png');
+        
+          // column.avatar = image
+        }
+        return column
+      })
+    })
+    return {
+      columnList
+    }
+  }
+});
 </script>
--- zheye/src/shims-vue.d.ts
+++ zheye/src/shims-vue.d.ts
@@ -3,4 +3,10 @@ declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
   export default component
+  declare function require(string): string;
+}
+declare function require(string): string;
+declare module "*.png" {
+  const value: any;
+  export = value;
 }
