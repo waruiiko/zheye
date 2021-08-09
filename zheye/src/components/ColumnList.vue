78aa39ef28e1fc4ed57411462c1a78a900f2b76e
GinBe <asdfwxwx@gmail.com>
2021-08-09 13:24:18 +0800

    Update ColumnList.vue

--- zheye/src/components/ColumnList.vue
+++ zheye/src/components/ColumnList.vue
@@ -19,7 +19,7 @@
   <!-- </div> -->
 </template>
 <script lang="ts">
-import { computed, defineComponent, PropType } from "vue";
+import { defineComponent, PropType,computed } from "vue";
 export interface ColumnProps {
   id: number;
   title: string;
@@ -28,29 +28,30 @@ export interface ColumnProps {
 }
 export default defineComponent({
   name: "ColumnList",
+  data() {
+    return {
+      a :1 
+    }
+  },
   props: {
     list: {
       //这里特别有一点，我们现在的 Array 是没有类型的，只是一个数组，我们希望它是一个 ColomnProps 的数组，那么我们是否可以使用了类型断言直接写成 ColomnProps[]，显然是不行的 ，因为 Array 是一个数组的构造函数不是类型，我们可以使用 PropType 这个方法，它接受一个泛型，讲 Array 构造函数返回传入的泛型类型。
       type: Array as PropType<ColumnProps[]>,
-      required: true,
+      required: true
     },
   },
   setup(props) {
-    console.log("s");
-    const columnList = computed(() => {
-      console.log("a")
-      return props.list.map((column) => {
-        console.log('1')
-        if (column.avatar == null) {
-          console.log("2")
-          column.avatar = require("../assets/120.jpg");
+    const columnList = computed(() => { 
+      return props.list.map(column => {
+        if (!column.avatar) {
+          column.avatar = require('@/assets/120.jpg')
         }
-        return column;
-      });
-    });
+        return column
+      })
+    })
     return {
-      columnList,
-    };
-  },
-});
+      columnList
+    }
+  }
+  })
 </script>
