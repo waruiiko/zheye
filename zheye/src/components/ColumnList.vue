0bdd9b58ddf844a4fb58d1c409a674d33fcfa36e
GinBe <asdfwxwx@gmail.com>
2021-08-16 16:23:50 +0800

    Update ColumnList.vue

--- zheye/src/components/ColumnList.vue
+++ zheye/src/components/ColumnList.vue
@@ -1,11 +1,15 @@
 <template>
-    <div class="row">
+  <div class="row">
     <div v-for="column in columnList" :key="column.id" class="col-4 mb-4">
       <div class="card h-100 shadow-sm">
         <div class="card-body text-center">
-          <img  :src="column.avatar" :alt="column.title" class="rounded-circle border border-light w-25 my-3" >
-          <h5 class="card-title">{{column.title}}</h5>
-          <p class="card-text text-left">{{column.description}}</p>
+          <img
+            :src="column.avatar"
+            :alt="column.title"
+            class="rounded-circle border border-light w-25 my-3"
+          />
+          <h5 class="card-title">{{ column.title }}</h5>
+          <p class="card-text text-left">{{ column.description }}</p>
           <a href="#" class="btn btn-outline-primary">进入专栏</a>
         </div>
       </div>
@@ -14,8 +18,9 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType ,computed} from "vue";
-import ColumnProps from '../types/ColumnProps'
+import { defineComponent, PropType, computed } from "vue";
+import ColumnProps from "../types/ColumnProps";
+declare function require(img: string): string;
 export default defineComponent({
   name: "ColumnList",
   props: {
@@ -27,7 +32,7 @@ export default defineComponent({
   },
   setup(props) {
     const columnList = computed(() => {
-      console.log('111')
+      console.log("111");
       return props.list.map((column) => {
         if (!column.avatar) {
           column.avatar = require("@/assets/ssb.jpg");
