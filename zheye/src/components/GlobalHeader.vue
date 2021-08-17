003a1db3aecdb5dd5294c8fd60ff543eb5581d07
GinBe <asdfwxwx@gmail.com>
2021-08-17 11:02:48 +0800

    Create GlobalHeader.vue

---
+++ zheye/src/components/GlobalHeader.vue
@@ -0,0 +1,28 @@
+<template>
+    <nav class="navbar navbar-dark bg-primary justify-content-between mb-4 px-4">
+</template>
+<script lang="ts">
+import { defineComponent,PropType } from 'vue'
+
+export interface UserProps{
+    isLogin:boolean;
+    name ?: string;
+    id ?: string
+}
+
+export default defineComponent({
+    name:'GlobalHeader',
+    props:{
+        user:{
+            type: Object as PropType<UserProps>,
+            required: true
+        }
+    },
+    setup() {
+        
+    },
+})
+</script>
+<style scoped>
+
+</style>
