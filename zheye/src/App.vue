ff80f2ee18f25d9662a9eff84997a436dc4592a8
GinBe <asdfwxwx@gmail.com>
2021-08-21 23:26:05 +0800

    form

--- zheye/src/App.vue
+++ zheye/src/App.vue
@@ -1,22 +1,51 @@
 <template>
   <div>
-    <GlobalHeader :user="user"/>
+    <GlobalHeader :user="user" />
+    <form>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <input
+          type="email"
+          class="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+          v-model="emailRef.val"
+          @blur="validateEmail"
+        />
+        <div id="emailHelp" class="form-text">
+          We'll never share your email with anyone else.
+        </div>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input
+          type="password"
+          class="form-control"
+          id="exampleInputPassword1"
+        />
+      </div>
+      <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
     <ColumnList :list="list" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent ,reactive } from "vue";
 import ColumnList from "./components/ColumnList.vue";
-import GlobalHeader,{UserProps} from "./components/GlobalHeader.vue"
+import GlobalHeader, { UserProps } from "./components/GlobalHeader.vue";
 import ColumnProps from "./hooks/ColumnProps";
 import "bootstrap/dist/css/bootstrap.min.css";
 declare function require(img: string): string;
 
-const user:UserProps = {
-  isLogin:true,
-  name:'xiaoming'
-}
+const user: UserProps = {
+  isLogin: false,
+  name: "xiaoming",
+};
 
 const testData: ColumnProps[] = [
   {
@@ -62,10 +91,22 @@ const testData: ColumnProps[] = [
 export default defineComponent({
   name: "App",
   components: {
-    ColumnList,GlobalHeader
+    ColumnList,
+    GlobalHeader,
   },
   setup() {
-    return { list: testData ,user};
+    const emailRef = reactive({
+      val:'',
+      error:false,
+      message:'',
+    })
+    const validateEmail =()=>{
+      if (emailRef.val.trim()== '') {
+        emailRef.error =true; 
+        emailRef.message = 'cant be empty';      
+      }
+    }
+    return { list: testData, user,emailRef,validateEmail };
   },
 });
 </script>
