687570537c850ba2a877a7b94a1c767da6fc819a
GinBe <asdfwxwx@gmail.com>
2021-08-21 20:07:31 +0800

    Update useClickOut.ts

--- zheye/src/hooks/useClickOut.ts
+++ zheye/src/hooks/useClickOut.ts
@@ -1,51 +1,25 @@
-// import { Ref, ref, onMounted, onUnmounted } from "vue";
-
-// const useClickOut = (elementRef: Ref<null | HTMLElement>) => {
-//     const isClickOut = ref(false);
-//     const handler = (e: MouseEvent) => {
-//         if (elementRef.value) {
-//             if (elementRef.value.contains(e.target as HTMLElement)) {
-//                 isClickOut.value = false
-//             }
-//         } else {
-//             isClickOut.value = true;
-//         }
-//     }
-//     //添加事件
-//     onMounted(() => {
-//         document.addEventListener("click", handler);
-//     }),
-//     //移除事件
-//     onUnmounted(() => {
-//         document.removeEventListener("click", handler);
-//     })
-//     return isClickOut
-// }
-
-// export default useClickOut
-
-import { ref, onMounted, onUnmounted, Ref } from 'vue'
+import { Ref, ref, onMounted, onUnmounted } from "vue";
 
 const useClickOut = (elementRef: Ref<null | HTMLElement>) => {
-  const isClickOutside = ref(false)
-  const handler = (e: MouseEvent) => {
-    if (elementRef.value) {
-      if (elementRef.value.contains(e.target as HTMLElement)) {
-        isClickOutside.value = false
-      } else {
-        isClickOutside.value = true
-      }
+    const isClickOut = ref(false);
+    const handler = (e: MouseEvent) => {
+        if (elementRef.value) {
+            if (elementRef.value.contains(e.target as HTMLElement)) {
+                isClickOut.value = false
+            }else {
+              isClickOut.value = true;
+          }
+        } 
     }
-  }
-  //添加事件
-  onMounted(() => {
-    document.addEventListener('click', handler)
-  })
-  //移除事件
-  onUnmounted(() => {
-    document.removeEventListener('click', handler)
-  })
-  return isClickOutside
+    //添加事件
+    onMounted(() => {
+        document.addEventListener("click", handler);
+    }),
+    //移除事件
+    onUnmounted(() => {
+        document.removeEventListener("click", handler);
+    })
+    return isClickOut
 }
 
 export default useClickOut
