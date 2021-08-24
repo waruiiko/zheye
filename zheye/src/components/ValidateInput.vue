0c4fb1e22b860c287cc47ad5a1a50efce486faa0
GinBe <asdfwxwx@gmail.com>
2021-08-24 11:47:18 +0800

    Update ValidateInput.vue

--- zheye/src/components/ValidateInput.vue
+++ zheye/src/components/ValidateInput.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="validate-input-container pb-3">
-    <input type="text"
+    <input
+      type="text"
       class="form-control"
-      :class="{'is-invalid': inputRef.error}"
+      :class="{ 'is-invalid': inputRef.error, 'is-valid': !inputRef.error }"
       v-model="inputRef.val"
       @blur="validateInput"
-    >
-    <span v-if="inputRef.error" class="invalid-feedback">{{inputRef.message}}</span>
+    />
+    <span v-if="inputRef.error" class="invalid-feedback">{{
+      inputRef.message
+    }}</span>
   </div>
 </template>
 
@@ -17,16 +20,18 @@ const emailReg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0
 
 export default defineComponent({
   props: {
-    rules: Array as PropType<RulesProp>
+    rules: Array as PropType<RulesProp>,
+    modelValue: String
   },
   setup(props) {
     const inputRef = reactive({
-      val: '',
+      val: props.modelValue ||'',
       error: false,
       message: ''
     })
     const validateInput = () => {
       if (props.rules) {
+        //every函数：只有当数组中每一项都为true，才返回true，否则返回false
         const allPassed = props.rules.every(rule => {
           let passed = true
           inputRef.message = rule.message
