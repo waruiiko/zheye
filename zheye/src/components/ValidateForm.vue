6db1864a23b4e3fa626e784a707f9ea3ab42a6ae
GinBe <asdfwxwx@gmail.com>
2021-08-26 17:17:58 +0800

    mitt

--- zheye/src/components/ValidateForm.vue
+++ zheye/src/components/ValidateForm.vue
@@ -12,22 +12,35 @@
 <script lang="ts">
 import { defineComponent, onUnmounted } from "vue";
 import mitt from "mitt";
+type ValidateFunc = () => boolean;
+// 实例化 mitt
 export const emitter = mitt();
 export default defineComponent({
   emits: ["form-submit"],
   setup(props, context) {
+    let funcArr: ValidateFunc[] = [];
     const submitForm = () => {
-      context.emit("form-submit", true);
+      // 循环执行数组 得到最后的验证结果
+      const result = funcArr.map((func) => func()).every((result) => result);
+      context.emit("form-submit", result);
     };
-
-    // const callback = (test: string) => {
-    //   console.log(test);
+    // 将监听得到的验证函数都存到一个数组中
+    // const callback = (func: ValidateFunc) => {
+    //   funcArr.push(func);
     // };
-    emitter.on("form-item-created", (test) => console.log("inputRef", test));
+    const callback = (func: any) => {
+      funcArr.push(func);
+    };
+    // 添加监听
+    emitter.on("form-item-created", callback);
     onUnmounted(() => {
-    emitter.off("form-item-created", (test) => console.log("foo", test));
+      // 删除监听
+      emitter.off("form-item-created", callback);
+      funcArr = [];
     });
-    return { submitForm };
+    return {
+      submitForm,
+    };
   },
 });
 </script>
