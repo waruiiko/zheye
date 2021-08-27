2ec949225f6589a1e2a13df960e1d48e5d0fc3c4
GinBe <asdfwxwx@gmail.com>
2021-08-27 15:54:00 +0800

    Update App.vue

--- zheye/src/App.vue
+++ zheye/src/App.vue
@@ -29,44 +29,7 @@
       <template #submit>
         <span class="btn btn-danger">submit</span>
       </template>
-    </validate-form>
-    <!-- <form>
-      <div class="mb-3">
-        <label for="validationServer01" class="form-label">First name</label>
-        <input
-          type="text"
-          class="form-control is-valid"
-          id="validationServer01"
-          required
-        />
-
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input
-          class="form-control"
-          type="email"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-          v-model="emailRef.val"
-          @blur="validateEmail"
-        />
-        <div id="emailHelp" class="form-text">
-          We'll never share your email with anyone else.
-        </div>
-      </div>
-      <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input
-          type="password"
-          class="form-control"
-          id="exampleInputPassword1"
-        />
-      </div>
-      <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-      </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
-    </form> -->
+    </validate-form> 
     <ColumnList :list="list" />
   </div>
 </template>
