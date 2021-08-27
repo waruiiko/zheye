5b7bda781a4908670d005fc1003a5f39aa18ed3c
GinBe <asdfwxwx@gmail.com>
2021-08-27 15:48:18 +0800

    router

--- zheye/src/main.ts
+++ zheye/src/main.ts
@@ -1,3 +1,25 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-createApp(App).mount('#app')
+
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from './views/Home.vue'
+import Login from './views/Login.vue'
+
+const routerHistory = createWebHistory()
+const router = createRouter({
+  history: routerHistory,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    }
+  ]
+})
+const app = createApp(App)
+app.mount('#app')
