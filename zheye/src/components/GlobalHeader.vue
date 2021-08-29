3c9d730b2587a52924a23bd433e8211a10d803aa
GinBe <asdfwxwx@gmail.com>
2021-08-29 16:55:12 +0800

    home

--- zheye/src/components/GlobalHeader.vue
+++ zheye/src/components/GlobalHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar navbar-dark bg-primary justify-content-between mb-4 px-4">
-    <a class="navbar-brand" href="#">支乎专栏</a>
+    <router-link class="navbar-brand" to="/home">支乎专栏</router-link>
     <ul v-if="!user.isLogin" class="list-inline mb-0">
       <li class="list-inline-item">
         <router-link to="/login" class="btn btn-outline-light my-2">登陆</router-link>
--- zheye/src/main.ts
+++ zheye/src/main.ts
@@ -10,7 +10,7 @@ const router = createRouter({
   history: routerHistory,
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'home',
       component: Home
     },
