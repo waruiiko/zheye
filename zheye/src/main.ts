aff374b31d92ee191ed2b081c38c37da9a2854d5
GinBe <asdfwxwx@gmail.com>
2021-09-01 15:58:52 +0800

    vuex

--- zheye/src/main.ts
+++ zheye/src/main.ts
@@ -5,6 +5,21 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from './views/Home.vue'
 import Login from './views/Login.vue'
 import Column from './views/ColumnDetail.vue'
+import {createStore}from 'vuex'
+
+const store = createStore({
+  state: {
+    count:0
+  },
+  mutations:{
+    add(state){
+      state.count++;
+    }
+  }
+})
+console.log("store",store.state.count)
+store.commit('add')
+console.log("store",store.state.count)
 
 const routerHistory = createWebHistory()
 const router = createRouter({
