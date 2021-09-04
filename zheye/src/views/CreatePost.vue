d689e6f2975f1ce63910349343107f3897750b82
GinBe <asdfwxwx@gmail.com>
2021-09-04 15:18:48 +0800

    Update CreatePost.vue

--- zheye/src/views/CreatePost.vue
+++ zheye/src/views/CreatePost.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
         <h4>新建文章</h4>
+        <form>
+            <div class="mb-3">
+                <label class="form-label">文章标题</label>
+                <input placeholder="title" type="text" name="title" />
+            </div>
+        </form>
     </div>
 </template>
 <script lang="ts">
