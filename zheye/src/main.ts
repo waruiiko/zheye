bc6c5809dc477f1cebe758ba398feca7e6f58a2e
GinBe <asdfwxwx@gmail.com>
2021-09-12 01:09:15 +0800

    Update main.ts

--- zheye/src/main.ts
+++ zheye/src/main.ts
@@ -4,8 +4,15 @@ import axios from 'axios'
 import router from './router'
 import store from './store'
 
-//设置基础url，来源：https://www.coingecko.com/zh/api/documentation
+//设置基础url，来源：https://www.coingecko.com/zh/api/documentation，原api:http://api.vikingship.xyz/public/swagger/index.html#/column 禁止了跨域请求
 axios.defaults.baseURL='https://api.coingecko.com/api/v3/coins/'
+//设置code，则axios.get('/columns?icode=123abc').then(...)这一句中'?icode=123abc'部分可以省略，由于此处没有icode，这段代码注释掉
+// axios.interceptors.request.use(config=>{
+//     config.params= {
+//         ...config.params,icode:'123abc'
+//     }
+//     return config
+// })
 axios.get('/bitcoin').then(resp=>{
     // console.log(resp)
     console.log(resp.data.market_data.current_price.usd)
