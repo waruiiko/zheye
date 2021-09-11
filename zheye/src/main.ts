154eafec395289237b551964079da8dfea8322d9
GinBe <asdfwxwx@gmail.com>
2021-09-12 00:57:48 +0800

    axios

--- zheye/src/main.ts
+++ zheye/src/main.ts
@@ -1,8 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import axios from 'axios'
 import router from './router'
 import store from './store'
+
+axios.defaults.baseURL='https://api.coingecko.com/api/v3/coins/'
+axios.get('/bitcoin').then(resp=>{
+    // console.log(resp)
+    console.log(resp.data.market_data.current_price.usd)
+})
 
 const app = createApp(App)
 app.use(router)
